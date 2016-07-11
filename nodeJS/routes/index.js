b82b282f3dc5d6682f4333f850cc458af1001d85
Stan Callewaert <callewaert.stan@gmail.com>
2016-07-11 17:37:20 +0200

    Change nodeJS to refactor for cachable queries

--- nodeJS/routes/index.js
+++ nodeJS/routes/index.js
@@ -14,8 +14,6 @@ router.get('/survey', function(req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
 
     var trainnumber = req.query.trainnumber;
-    var from = req.query.from;
-    var to = req.query.to;
 
     MongoClient.connect(url, function(err, db) {
         if(err) {
@@ -24,27 +22,11 @@ router.get('/survey', function(req, res) {
             var collection = db.collection('survey');
 
             if(trainnumber) {
-                if(from && to) {
-                    //.limit(1)!!!!!!!!!!!
-                    collection.find({"trainnumber": trainnumber, "from": from}).toArray(function (errorFrom, resultFrom) {
-                        collection.find({"trainnumber": trainnumber, "to": to}).toArray(function (errorTo, resultTo) {
-                            var connectionFrom = getConnectionNumber(resultFrom);
-                            var connectionEnd = getConnectionNumber(resultTo);
-
-                            collection.find({"trainnumber": trainnumber, "connectionnumber": { $gte: connectionFrom, $lte: connectionEnd }}).toArray(function (err, result) {
-                                processResult(err, result, res, function () {
-                                    db.close();
-                                });
-                            });
-                        });
+                collection.find({"trainnumber": trainnumber}).toArray(function (err, result) {
+                    processResult(err, result, res, function () {
+                        db.close();
                     });
-                } else {
-                    collection.find({"trainnumber": trainnumber}).toArray(function (err, result) {
-                        processResult(err, result, res, function () {
-                            db.close();
-                        });
-                    });
-                }
+                });
             } else {
                 collection.find({}).toArray(function (err, result) {
                     processResult(err, result, res, function () {
@@ -60,22 +42,12 @@ var processResult = function(err, result, res, callback) {
     if (err) {
         res.send(err);
     } else if (result.length) {
-        //console.log(result);
         res.end(JSON.stringify(result));
     } else {
         res.send("Nothing found");
     }
 
     callback();
-};
-
-var getConnectionNumber = function(result) {
-    if (result.length) {
-        console.log(result[0]["connectionnumber"]);
-        return result[0]["connectionnumber"];
-    } else {
-        //Nothing
-    }
 };
 
 module.exports = router;
